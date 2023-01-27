f4d5254c97be02962a364ede88866a4a2d9aa275
Alexander <mutilatorap@gmail.com>
2023-01-26 21:20:10 -0600

    Change questions

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,20 +1,12 @@
 const images = {
-  '38': require('../assets/images/questions/38.png'),
-  '49': require('../assets/images/questions/49.png'),
-  '53': require('../assets/images/questions/53.png'),
-  '54': require('../assets/images/questions/54.png'),
-  '55': require('../assets/images/questions/55.png'),
-  '58': require('../assets/images/questions/58.png'),
-  '59': require('../assets/images/questions/59.png'),
-  '60': require('../assets/images/questions/60.png'),
-  '64': require('../assets/images/questions/64.png'),
-  '65': require('../assets/images/questions/65.png'),
-  '67': require('../assets/images/questions/67.png'),
-  '68': require('../assets/images/questions/68.png'),
-  '69': require('../assets/images/questions/69.png'),
-  '70': require('../assets/images/questions/70.png'),
-  '71': require('../assets/images/questions/71.png'),
-  '72': require('../assets/images/questions/72.png'),
+  '1': require('../assets/images/questions/1.png'),
+  '2': require('../assets/images/questions/2.png'),
+  '3': require('../assets/images/questions/3.png'),
+  '4': require('../assets/images/questions/4.png'),
+  '5': require('../assets/images/questions/5.png'),
+  '6': require('../assets/images/questions/6.png'),
+  '7': require('../assets/images/questions/7.png'),
+  '8': require('../assets/images/questions/8.png'),
 };
 
 export default images;
